89ebd80200a000ff4bced6d5bb53501c63419f95
hilmarm <hilmarm@stud.ntnu.no>
2016-07-11 11:01:50 -0300

    added header and cpp file for future trust region algorithm.
    The method will be one described in Caio Giuliani's paper, and similarily in Conn's book Introdcution To Derivative Free Methods

---
+++ FieldOpt/Optimization/optimizers/trust_region_derivative.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hilmarm on 30.06.16.
+//
+
+#include "trust_region_derivative.h"
---
+++ FieldOpt/Optimization/optimizers/trust_region_derivative.h
@@ -0,0 +1,58 @@
+//
+// Created by hilmarm on 30.06.16.
+//
+
+#ifndef TRUSTREGIONDERIVATIVE_H
+#define TRUSTREGIONDERIVATIVE_H
+
+#include "Optimization/optimizer.h"
+
+namespace Optimization { namespace Optimizers {
+
+/*!
+ * \brief The CompassSearch class is an implementation of the Compass Search optimization algorithm
+ * described by Torczon, Lewis and Kolda.
+ *
+ * This algorithm only supports integer and real variables.
+ *
+ * Reference:
+ *
+ * Kolda, Tamara G., Robert Michael Lewis, and Virginia Torczon.
+ *  "Optimization by direct search: New perspectives on some classical and modern methods."
+ *  SIAM review 45.3 (2003): 385-482.
+ */
+        class TrustRegionDerivative : public Optimizer
+        {
+        public:
+            TrustRegionDerivative(::Utilities::Settings::Optimizer *settings, Case *base_case, ::Model::Properties::VariablePropertyContainer *variables);
+            double step_length() const { return step_length_; }
+
+        private:
+            double step_length_; //!< The size of the perturbation for each variable.
+            double minimum_step_length_; //!< Smallest allowed step length for the optimizer. _This is a termination condition_.
+
+            void step(); //!< Move to a new tentative best case found in the list of recently evaluated cases.
+            void contract(); //!< Contract the pattern, i.e. reduce the step length.
+            void perturb(); //!< Apply the pattern to generate new cases.
+
+            // Optimizer interface
+        public:
+            /*!
+             * \brief IsFinished Check if the optimization is finished.
+             *
+             * This algorithm has two termination conditions: max number of objective function evaluations and
+             * minimum step length.
+             * \return True if the algorithm has finished, otherwise false.
+             */
+            bool IsFinished();
+
+            QString GetStatusStringHeader() const;
+            QString GetStatusString() const;
+
+        private:
+            void iterate(); //!< Step or contract, perturb, and clear list of recently evaluated cases.
+        };
+
+    }}
+
+#endif // COMPASSSEARCH_H
